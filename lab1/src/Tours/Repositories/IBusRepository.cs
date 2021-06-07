b7ba991630a4d575131820b8e662f5ca77749ac5
platonovaos <66078263+platonovaos@users.noreply.github.com>
2021-06-07 21:30:17 +0300

    final vers

---
+++ lab1/src/Tours/Repositories/IBusRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tours
+{
+    public interface IBusRepository : CrudRepository<Busticket, int>
+    {
+        List<Busticket> FindBusByLowCost(int cost);
+    }
+
+}
