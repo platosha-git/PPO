c3ed164f1cbff63f279d6643f96f0490fb49569b
platonovaos <66078263+platonovaos@users.noreply.github.com>
2021-06-08 12:56:51 +0300

    i think its final 1

--- creationDB/insert.sql
+++ creationDB/insert.sql
@@ -1,3 +1,16 @@
+/*INSERT INTO Users (UserID, AccessLevel, Name, Surname, Year) VALUES 
+(1, 0, 'Ivan', 'Ivanov', 1991),
+(2, 1, 'Sergei', 'Petrov', 1992),
+(3, 2, 'Denis', 'Sklifasovskii', 1990),
+(4, 1, 'Pavel', 'Khetagurov', 1995),
+(5, 2, 'Daniil', 'Suslikov', 1997),
+(6, 0, 'Daria', 'Rusinova', 1999),
+(7, 0, 'Maxim', 'Kozlov', 1998),
+(8, 1, 'Milana', 'Sarkisyan', 1995),
+(9, 1, 'Victor', 'Goryachev', 1994),
+(10, 0, 'Dmitrii', 'Zhigalkin', 1993);
+*/
+
 /*INSERT INTO Booking (Customer, TourID, Login, Password) VALUES 
 (1, '{}', '', ''),
 (2, '{1, 4}', 'SergeiSinp', 'SPetrov15'),
@@ -9,18 +22,6 @@
 (8, '{3}', 'MilaUkn', 'SarkisMil'),
 (9, '{6}', 'GorVictor', 'Victor94'),
 (10, '{}', '', '');
-
-INSERT INTO Users (UserID, AccessLevel, Name, Surname, Year) VALUES 
-(1, 0, 'Ivan', 'Ivanov', 1991),
-(2, 1, 'Sergei', 'Petrov', 1992),
-(3, 2, 'Denis', 'Sklifasovskii', 1990),
-(4, 1, 'Pavel', 'Khetagurov', 1995),
-(5, 2, 'Daniil', 'Suslikov', 1997),
-(6, 0, 'Daria', 'Rusinova', 1999),
-(7, 0, 'Maxim', 'Kozlov', 1998),
-(8, 1, 'Milana', 'Sarkisyan', 1995),
-(9, 1, 'Victor', 'Goryachev', 1994),
-(10, 0, 'Dmitrii', 'Zhigalkin', 1993);
 */
 
 /*INSERT INTO City (CityID, Name) VALUES
@@ -62,18 +63,18 @@ INSERT INTO Users (UserID, AccessLevel, Name, Surname, Year) VALUES
 (10, 7, 'Walgreens', 'Hostel', 5, TRUE, 6402);
 */
 
-/*INSERT INTO BusTicket (BusTID, Seat, CityFrom, CityTo, DepartureTime, ArrivalTime, Luggage, Cost, Bus) VALUES
-(0, 0, 1, 2, '00:00:00', '00:00:00', FALSE, 0, 0),
-(1, 45, 4, 1, '10:23:00', '12:45:00', FALSE, 23012, 234),
-(2, 34, 6, 8, '10:00:00', '11:45:00', TRUE, 14902, 174),
-(3, 12, 1, 3, '01:13:00', '02:31:00', FALSE, 10029, 52),
-(4, 9, 8, 7, '15:45:00', '17:04:00', FALSE, 7901, 5),
-(5, 72, 9, 1, '09:31:00', '11:10:00', TRUE, 8102, 70),
-(6, 3, 10, 3, '17:50:00', '18:53:00', TRUE, 15741, 41),
-(7, 27, 2, 4, '06:29:00', '08:14:00', FALSE, 6204, 92),
-(8, 10, 5, 9, '21:47:00', '23:07:00', TRUE, 3921, 13),
-(9, 64, 3, 2, '19:56:00', '21:12:00', FALSE, 6193, 58),
-(10, 71, 8, 1, '22:30:00', '23:58:00', TRUE, 5825, 48);
+/*INSERT INTO BusTicket (BusTID, Bus, Seat, CityFrom, CityTo, DepartureTime, ArrivalTime, Luggage, Cost) VALUES
+(0, 0, 0, 1, 2, '00:00:00', '00:00:00', FALSE, 0),
+(1, 234, 45, 4, 1, '10:23:00', '12:45:00', FALSE, 23012),
+(2, 174, 34, 6, 8, '10:00:00', '11:45:00', TRUE, 14902),
+(3, 52, 12, 1, 3, '01:13:00', '02:31:00', FALSE, 10029),
+(4, 5, 9, 8, 7, '15:45:00', '17:04:00', FALSE, 7901),
+(5, 70, 72, 9, 1, '09:31:00', '11:10:00', TRUE, 8102),
+(6, 41, 3, 10, 3, '17:50:00', '18:53:00', TRUE, 15741),
+(7, 92, 27, 2, 4, '06:29:00', '08:14:00', FALSE, 6204),
+(8, 13, 10, 5, 9, '21:47:00', '23:07:00', TRUE, 3921),
+(9, 58, 64, 3, 2, '19:56:00', '21:12:00', FALSE, 6193),
+(10, 48, 71, 8, 1, '22:30:00', '23:58:00', TRUE, 5825);
 */
 
 /*INSERT INTO PlaneTicket (PlaneTID, Plane, Seat, Class, CityFrom, CityTo, DepartureTime, Luggage, Cost) VALUES
@@ -129,11 +130,6 @@ INSERT INTO Users (UserID, AccessLevel, Name, Surname, Year) VALUES
 (9,	10,	8,	9, 82346,	'2021-04-17',	'2021-09-14'),
 (10, 1,	7,	8, 42921,	'2020-10-09',	'2021-11-16');
 */
-
-
-
-
-
 
 
 
