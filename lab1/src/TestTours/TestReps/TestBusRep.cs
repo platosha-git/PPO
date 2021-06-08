66152e71de65a0172cc244d362126f1e671abb26
platonovaos <66078263+platonovaos@users.noreply.github.com>
2021-06-08 14:20:29 +0300

    final before check

--- lab1/src/TestTours/TestReps/TestBusRep.cs
+++ lab1/src/TestTours/TestReps/TestBusRep.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tours;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace TestTours
 {
@@ -21,44 +22,36 @@ namespace TestTours
         public void TestFindById()
         {
             Busticket busF = BusRep.FindByID(1);
-            Assert.AreEqual(busF.Cost, 23012);
+            Assert.AreEqual(busF.Bus, 234);
         }
 
         [TestMethod]
-        public void TestAdd()
+        public void TestAUD()
         {
-            Busticket busA = new Busticket { Bustid = 11, Seat = 23, Cityfrom = 3, Cityto = 8, Luggage = true, Cost = 9201 };
+            DateTime timeB = new DateTime(2021, 01, 01, 13, 15, 00);
+            DateTime timeE = new DateTime(2021, 01, 01, 15, 37, 00);
+            Busticket busA = new Busticket { Bustid = 11, Bus = 74, Seat = 10, Cityfrom = 7, Cityto = 2, 
+                Departuretime = timeB.TimeOfDay, Arrivaltime = timeE.TimeOfDay, Luggage = true, Cost = 4302 };
             BusRep.Add(busA);
-            Busticket busF = BusRep.FindAll().Last();
-            Assert.AreEqual(busA, busF);
-        }
-
-        /*[TestMethod]
-        public void TestUpdate()
-        {
-            Busticket busU = new Busticket { Seat = 23, Cityfrom = 3, Cityto = 8, Luggage = false, Cost = 9201 };
-            BusRep.Update(busU);
-
-            Busticket busF = BusRep.FindAll().Last();
-            Assert.AreEqual(busU, busF);
-        }
-        
-
-        [TestMethod]
-        public void TestDeleteById()
-        {
+            Busticket tourFA = BusRep.FindAll().Last();
+            Assert.AreEqual(busA, tourFA);
+            
+            busA.Luggage = false;
+            BusRep.Update(busA);
+            Busticket tourFU = BusRep.FindAll().Last();
+            Assert.AreEqual(busA, tourFU);
+            
             int LastID = BusRep.FindAll().Count();
-            BusRep.DeleteByID(12);
-
-            Assert.AreEqual(LastID, 12);
+            BusRep.DeleteByID(LastID);
+            int NLastID = BusRep.FindAll().Count();
+            Assert.AreEqual(LastID - 1, NLastID);
         }
-        */
 
         [TestMethod]
         public void TestFindBusByLowCost()
         {
             List<Busticket> buses = BusRep.FindBusByLowCost(7000);
-            Assert.IsNotNull(buses);
+            Assert.IsTrue(buses.Count != 0);
         }
     }
 }
